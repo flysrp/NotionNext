1d0b76c4de65161a79faaf46f73fdbaae5c45490
flysrp <rupengsong@gmail.com>
2025-02-12 14:11:08 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Orange', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '所思不远，若为平生', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'photo.orange42.cc', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'word.orange42.cc', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '博客, 摄影', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
